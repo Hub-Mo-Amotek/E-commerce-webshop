3fe0d20f1f68cf6c8ea17d6476502224a52159c1
Hub-Mo <Mohamed.elja@hotmail.com>
2022-08-19 11:39:13 +0200

    added Routes to app.js and added pages for frontend

---
+++ webshop-frontend/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+
+
+const Header = () => {
+  return (
+    <div>Header
+        <NavBar />
+    </div>
+  )
+}
+
+export default Header
