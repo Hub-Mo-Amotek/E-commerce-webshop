0109d2bd26dcac48f8d0de4a22cb598792b61714
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-08-20 12:15:00 +0200

    edited the navbar, added the options

--- webshop-frontend/src/components/NavBar.js
+++ webshop-frontend/src/components/NavBar.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <div className="navbar">
+            <div className="navbar_option1">
+              <span className="login">Log In</span>
+            </div>
+
+            <div className="navbar_option2">
+                    <span className="signup">Sign Up</span>
+            </div>
+
+            <div className="navbar_option3">
+                    <span className="cart">Shopping Cart</span>
+            </div>
+
+            <div className="navbar_option4">
+                    <span className="sell">Sell</span>
+            </div>
+
+            <div className="navbar_option5">
+                    <span className="products">All Products</span>
+            </div>
+
+            <div className="navbar_option6">
+                    <span className="categories">Categories</span>
+            </div>
+
+            <div className="navbar_option7">
+                    <span className="logout">Log Out</span>
+            </div>
+        </div>
+    )
 }
 
 export default NavBar
