b835242bea7d86e4ed5f1a56ac8ec3f32db97121
Hub-Mo <Mohamed.elja@hotmail.com>
2022-08-20 21:43:43 +0200

    added acces headers and added the right url for private API

--- server/index.php
+++ server/index.php
@@ -1,9 +1,13 @@
 
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
     // allowing acces from the front end with these, we should specify this later for security purposes.
-    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Origin: http://localhost:3000");
     header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Methods: *");
+
+
 
     // .env stuff
     require_once realpath(__DIR__ . "/vendor/autoload.php");
@@ -16,7 +20,7 @@
     require 'Model/DataConnection.php';
 
 
-    // here we are calling the render function insite the home controller
+    // here we are calling the render function inside the home controller
     $HomeController = new HomeController();
     $HomeController->render();
 
