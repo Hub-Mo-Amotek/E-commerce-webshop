3b54110ff1d342dccbdb82a19e68ace5d48f3ff6
Wenke Quarteer <wendyquarteer@outlook.com>
2022-08-22 12:56:16 +0200

    able to add new products (hardcode)

--- webshop-frontend/src/routes/CartPage.js
+++ webshop-frontend/src/routes/CartPage.js
@@ -1,7 +1,47 @@
+import React,{useState} from "react";
+import newProducts from "../components/homepage/NewProducts";
 const CartPage = () => {
-    function handleRemove() {
-        // remove item
+
+
+    //at the same time the subtotal in the order summary will be equal to the price of that product.
+    //the shipping cost wil be displayed underneath.
+    //if the seller is the same person the shipping cost will not add-up when adding more of his products.
+    //The order-total is equal to the subtitle + shipping (costs).
+
+    //TODO-1 When the user clicks on "add to cart" the item with that specific product.id will be added to the cart(1 piece only).
+    //id + imageSrc + imageAlt + name + price + shipping cost + quantity (=1)
+    const productsInCartList = [
+        {
+            id: 111,
+            imageAlt: 'Pikachu',
+            imageSrc:'#',
+            name: 'pikachu',
+            price: 111,
+            shippingCost: 111,
+            quantity: 1,
+        },
+        {
+            id: 211,
+            imageAlt: 'Lala',
+            imageSrc:'#',
+            name: 'lala',
+            price: 211,
+            shippingCost: 111,
+            quantity: 1,
+        }
+    ];
+
+    //TODO: DONE--In order to modify the list, we need to use the useState Hook:
+    const [products, setProducts] = useState(productsInCartList);
+    console.log ("products: " ,products)
+
+    //TODO: DONE--When the user click on the remove-button, the item with that specific product.id will be removed from the cart.
+    //the id will be filtered out of the list and a new list will be displayed.
+    function handleRemove(id) { //line 78 = delete-button
+        const newProducts = products.filter((product) => product.id !== id);
+        setProducts(newProducts);//state not updating?  LOOK HERE!
     }
+    console.log("newProducts: " ,newProducts)
     return (
         <div className="cartPage">
             <div className="max-w-2xl mx-auto pt-16 pb-24 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -11,41 +51,42 @@ const CartPage = () => {
                     <section aria-labelledby="cart-heading" className="lg:col-span-7">
 
                         <ul role="list" className="border-t border-b border-gray-200 divide-y divide-gray-200">
-                            {/*products.map((product, productIdx) => (*/
-                                <li /*key={product.id}*/ className="flex py-6 sm:py-6">
+                            {products.map((product, key) => (
+                                <li key={key} className="flex py-6 sm:py-6">
+                                    {/*IMAGE*/}
                                     <div className="flex-shrink-0">
-                                        {/*IMAGE*/}
                                         <img
-                                            src="./assets/images/pikachu terrarium.jpeg"/*product.imageSrc*/
-                                            alt="Pikachu"/*product.imageAlt*/
+                                            src={product.imageSrc}
+                                            alt={product.imageAlt}
                                             className="w-14 h-14 object-center object-cover sm:w-28 sm:h-28"
                                         />
                                     </div>
-
+                                    {/*NAME + PRICE + QUANTITY + REMOVE*/}
                                     <div className="ml-4 flex-1 flex flex-col justify-between sm:ml-6">
                                         <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
-                                                <div className="flex justify-between">
-                                                    {/*NAME*/}
-                                                    <p className="text-sm">
+                                            {/*NAME & PRICE*/}
+                                            <div className="flex justify-between">
+                                                {/*NAME*/}
+                                                <p className="text-sm">
                                                         <a href="#"/*product.href*/
                                                            className="font-medium text-gray-700 hover:text-gray-800">
-                                                            Pikachu terrarium{/*product.name*/}
+                                                            {product.name}
                                                         </a>
                                                     </p>
                                                     {/*PRICE*/}
-                                                    <p className="mt-1 text-sm font-medium text-gray-900">€10.00{/*product.price*/}</p>
+                                                    <p className="mt-1 text-sm font-medium text-gray-900">{product.price}</p>
                                                 </div>
-
+                                            {/*QUANTITY & REMOVE*/}
                                             <div className="mt-4 sm:mt-0 sm:pr-9">
+                                                {/*QUANTITY*/}
                                                 <label /*htmlFor={`quantity-${productIdx}`}*/ className="sr-only">
-                                                    {/*Quantity, {product.name}*/}
+                                                    {/*quantity, {product.name}*/}
                                                 </label>
                                                 <select
                                                     /*id={`quantity-${productIdx}`}*/
                                                     /*name={`quantity-${productIdx}`}*/
                                                     className="max-w-full rounded-md border border-gray-300 py-1.5 text-base leading-5 font-medium text-gray-700 text-left shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                                 >
-                                                    {/*QUANTITY*/}
                                                     <option value={1}>1</option>
                                                     <option value={2}>2</option>
                                                     <option value={3}>3</option>
@@ -55,9 +96,9 @@ const CartPage = () => {
                                                     <option value={7}>7</option>
                                                     <option value={8}>8</option>
                                                 </select>
-
+                                                {/*REMOVE*/}
                                                 <div className="absolute top-0 right-0">
-                                                    <button type="button"  onClick={handleRemove}
+                                                    <button type="button"  onClick={() => handleRemove(products.id)}
                                                             className="-m-2 p-2 inline-flex text-gray-400 hover:text-gray-500">
                                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"
                                                              fill="none" viewBox="0 0 24 24" stroke="red"
@@ -71,7 +112,7 @@ const CartPage = () => {
                                         </div>
                                     </div>
                                 </li>
-                           /* ))*/}
+                           ))}
                         </ul>
                     </section>
 
