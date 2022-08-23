3e570872004c27be7a9ae21b709ba9ee8d94fe00
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-23 10:20:13 +0200

    testing how to place div at bottom with tailwind

--- webshop-frontend/src/components/Header.js
+++ webshop-frontend/src/components/Header.js
@@ -5,13 +5,21 @@ import NavBar from './NavBar';
 
 const Header = () => {
   return (
-    <div className="flex flex-col p-5 w-full h-96 bg-gradient-to-t from-color-one to-color-two">
-        <div className="flex items-center">
-            header
+    <div>
+        <div className="flex flex-col p-5 w-full h-96 bg-gradient-to-t from-color-one to-color-two">
+            <div className="p-10 w-full">
+                header
+            </div>
+
+            <div className="p-10 mt-auto w-full">
+                bottom test
+            </div>
         </div>
 
         <NavBar />
+
     </div>
+
   )
 }
 
