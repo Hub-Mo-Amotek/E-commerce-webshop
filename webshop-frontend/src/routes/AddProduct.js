945114def7d1cc3143562345a9ed63fb47364e07
Hub-Mo <Mohamed.elja@hotmail.com>
2022-08-24 15:00:31 +0200

    solving errors

--- webshop-frontend/src/routes/AddProduct.js
+++ webshop-frontend/src/routes/AddProduct.js
@@ -3,20 +3,22 @@ import React, { useState } from 'react';
 
 const AddProduct = () => {
     const [inputs, setInputs] = useState([]);
-    const [file, setFile] = useState();
+    // const [file, setFile] = useState();
    
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        if(name !== 'primary_image'){
-            setInputs(values => ({...values, [name]: value}));
-        } else{
-            setFile(URL.createObjectURL(event.target.files[0]));
-            const name1 = event.target.name;
-            console.log(file);
-            setInputs(values => ({...values, [name1]: file}));
+        setInputs(values => ({...values, [name]: value}));
+
+        // if(name !== 'primary_image'){
+        //     setInputs(values => ({...values, [name]: value}));
+        // } else{
+        //     setFile(URL.createObjectURL(event.target.files[0]));
+        //     const name1 = event.target.name;
+        //     console.log(file);
+        //     setInputs(values => ({...values, [name1]: file}));
             
-        }
+        // }
     }
 
 
@@ -150,7 +152,6 @@ const AddProduct = () => {
                     <button type="submit" name="page">Submit</button>
                 </form>
             </div>
-            <img src={file}  alt={file}/>
         </div>
     </div>
   )
