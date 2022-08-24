b58199a0b2d9e361ae0a46b4bc4ea8f5f74d66a0
Wenke Quarteer <wendyquarteer@outlook.com>
2022-08-24 10:56:57 +0200

    commented navbar

--- webshop-frontend/src/components/NavBar.js
+++ webshop-frontend/src/components/NavBar.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react'
 import { useEffect, useRef, useState } from 'react'
 import {Routes, Route, useNavigate} from 'react-router-dom';
@@ -116,3 +117,4 @@ export default () => {
             </nav>
         )
 }
+*/
