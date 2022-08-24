fc67101d76160450443b1cea9e0e3f170b4f6074
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-08-24 10:30:55 +0200

    Add popperJS

--- webshop-frontend/src/routes/AddProduct.js
+++ webshop-frontend/src/routes/AddProduct.js
@@ -10,15 +10,28 @@ const AddProduct = () => {
         setInputs(values => ({...values, [name]: value}));
     }
 
+    const handleImage = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+
+
     const handleForm = (e) => {
         e.preventDefault();
 
-        axios.post('http://localhost/E-commerce-webshop/server/', inputs)
+        axios.post('http://localhost/E-commerce-webshop/server/Model/Products.php', inputs)
         console.log(inputs)
     }
 
   return (
     <div className='AddProduct'>
+        
+
+
+
+
       	<div className="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
 		    <div className="max-w-md w-full">
                 <div>
@@ -27,80 +40,100 @@ const AddProduct = () => {
                         Add new product for sell
                     </h3>
                 </div>
-                <form className="mt-8 space-y-6" method="POST" action="/example.php" onSubmit={ handleForm }>
+                <form className="mt-8 space-y-6" onSubmit={ handleForm }>
                     {/* product Name */}
                     <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
-                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="product-name">
+                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="name">
                             Product Name
                         </label>
                         </div>
                         <div className="md:w-2/3">
-                        <input className="shadow bg-gray-200 appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="product-name" name="product-name" type="text" placeholder="Product Name" onChange={handleChange} required/>
+                        <input className="shadow bg-gray-200 appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="name" name="name" type="text" placeholder="Product Name" onChange={handleChange} required/>
                         </div>
                     </div>
 
                     {/* product Description */}
                     <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
-                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="product-description">
+                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="description">
                             Product Description
                         </label>
                         </div>
                         <div className="md:w-2/3">
-                        <textarea id="product-description" name="product-description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Product description" onChange={handleChange} ></textarea>
+                        <textarea id="description" name="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Product description" onChange={handleChange} ></textarea>
                         </div>
                     </div>
                     {/* product short description */}
                     <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
-                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="product-short-description">
+                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="short_description">
                             Product Short Description
                         </label>
                         </div>
                         <div className="md:w-2/3">
-                       <input className="shadow bg-gray-200 appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="product-short-description" name="product-short-description" type="text" placeholder="Product Short Description" onChange={handleChange} required/>
+                       <input className="shadow bg-gray-200 appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="short_description" name="short_description" type="text" placeholder="Product Short Description" onChange={handleChange} required/>
                         </div>
                     </div>
                     {/* product Price */}
                     <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
-                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="product-price">
+                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="price">
                             Product Price
                         </label>
                         </div>
                         <div className="md:w-2/3">
-                       <input className="shadow bg-gray-200 appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="product-product" name="product-price" type="number" placeholder="Product Price" onChange={handleChange} required/>
+                       <input className="shadow bg-gray-200 appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="price" name="price" type="number" placeholder="Product Price" onChange={handleChange} required/>
                         </div>
                     </div>
                     {/* product Images */}
                     <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
-                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor="product-image">Upload file</label>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor="primary_image">Upload file</label>
                     </div>
                     <div className="md:w-2/3">
-                    <input className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="product-image-help" id="product-image" name="product-image" type="file" accept="image/x-png,image/gif,image/jpeg" onChange={handleChange}/>
+                    <input className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="primary_image-help" id="primary_image" name="primary_image" type="file" accept="image/x-png,image/gif,image/jpeg" onChange={handleChange}/>
+                   
+                    </div>
+                    </div>
+                    {/* product Sub Images */}
+                    <div className="md:flex md:items-center mb-6">
+                        <div className="md:w-1/3">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor="sub_image_1">Upload file</label>
+                    </div>
+                    <div className="md:w-2/3">
+                    <input className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="sub_image_1_help" id="sub_image_1" name="sub_image_1" type="file" accept="image/x-png,image/gif,image/jpeg" onChange={handleChange}/>
+                   
+                    </div>
+                    </div>
+                    {/* product sub Images2 */}
+                    <div className="md:flex md:items-center mb-6">
+                        <div className="md:w-1/3">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor="sub_image_2">Upload file</label>
+                    </div>
+                    <div className="md:w-2/3">
+                    <input className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="sub_image_2_help" id="sub_image_2" name="sub_image_2" type="file" accept="image/x-png,image/gif,image/jpeg" onChange={handleChange}/>
                    
                     </div>
                     </div>
                     {/* product Quantity */}
                     <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
-                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="product-quantity">
+                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="quantity">
                             Product Quantity
                         </label>
                         </div>
                         <div className="md:w-2/3">
-                       <input className="shadow bg-gray-200 appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="product-product" name="product-quantity" type="text" placeholder="Product Quantity" onChange={handleChange} required/>
+                       <input className="shadow bg-gray-200 appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="quantity" name="quantity" type="text" placeholder="Product Quantity" onChange={handleChange} required/>
                         </div>
                     </div>
                     {/* product category dropdown */}
                     <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
-                            <label htmlFor="categories" className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">Select a category</label>
+                            <label htmlFor="category_id" className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">Select a category</label>
                         </div>
                         <div className="md:w-2/3">
-                            <select id="categories" className="shadow bg-gray-200 appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" name="categories" onChange={handleChange} required>
+                            <select id="category_id" className="shadow bg-gray-200 appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" name="category_id" onChange={handleChange} required>
                                 <option defaultValue>Choose a categories</option>
                                 <option value="1">XBox</option>
                                 <option value="2">Play Station</option>
@@ -111,8 +144,9 @@ const AddProduct = () => {
                         </div>
                     </div>
                     {/* product inserted user id */}
-                    <input type="hidden" name="user-id" value="1" onChange={handleChange}></input>
-                    <button type="submit" name="page" value="addProduct">Submit</button>
+                    <input type="text" name="user_id"  onChange={handleChange}></input>
+                    <input type="text" name="discount_id" onChange={handleChange}></input>
+                    <button type="submit" name="page">Submit</button>
                 </form>
             </div>
         </div>
