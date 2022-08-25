2b1a72943b1cbb33b18e0026d9bb8f27f31c4bcd
Wenke Quarteer <wendyquarteer@outlook.com>
2022-08-25 14:53:40 +0200

    prepared for add-to-cart-funtionality

--- webshop-frontend/src/components/homepage/NewProducts.js
+++ webshop-frontend/src/components/homepage/NewProducts.js
@@ -91,7 +91,7 @@ const NewProducts = () => {
 
                                 {/*BUTTON ADD TO CART*/}
                                 <button type="submit" className="add-to-cart flex items-center justify-center
-                                            w-full px-8 py-4 mt-4 rounded-sm bg-yellow-500"> onClick={() => addToCart(product.id)}
+                                            w-full px-8 py-4 mt-4 rounded-sm bg-yellow-500" onClick={() => addToCart(product.id)}>
                                     <span className="text-sm font-medium">
                                     Add to Cart
                                     </span>
