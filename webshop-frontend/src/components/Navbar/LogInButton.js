c5e443932a3dfd9032c6c4f055b10ebc67901c53
Hub-Mo <Mohamed.elja@hotmail.com>
2022-08-25 12:12:30 +0200

    implemented login logic in the navbar

--- webshop-frontend/src/components/Navbar/LogInButton.js
+++ webshop-frontend/src/components/Navbar/LogInButton.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import { useEffect,useState } from 'react'
+import {useNavigate} from 'react-router-dom';
+
+
 
 const LogInButton = () => {
+    let navigate = useNavigate();
+
+
+    const [authe, setAuthe] = useState();
+    const [name, setName] = useState();
+    let [log, setLog] = useState();
+
+
+
+    useEffect(()=>{
+            let auth = localStorage.getItem('id');
+            let b = localStorage.getItem('firstName')
+
+            if(auth){
+                    setLog('Logout');
+                  }else{
+                    setLog('Login');
+                  }
+
+            setAuthe(auth);
+            setName(b);
+    }, []);
+
+    const handleLogAction = () => {
+            let auth = localStorage.getItem('id');
+            if(auth){
+                    localStorage.clear();
+                    navigate('/Login');
+                  }else{
+                    navigate('/Login');
+                  }
+    }
+
     return (
         <button type="button"
-                className="inline-flex items-center px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-md">
-            Login
+                className="inline-flex items-center px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-md"
+                onClick={handleLogAction}>
+            {log}
         </button>
     )
 }
