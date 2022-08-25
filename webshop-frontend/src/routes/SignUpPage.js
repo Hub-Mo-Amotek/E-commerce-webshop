28c9d6906f4e899e31af187d0dcffa56ba13adcb
Hub-Mo <Mohamed.elja@hotmail.com>
2022-08-25 21:05:21 +0200

    fixed the button bug in the signup page

--- webshop-frontend/src/routes/SignUpPage.js
+++ webshop-frontend/src/routes/SignUpPage.js
@@ -68,7 +68,9 @@ const SignUpPage = () => {
                             onChange={handleChange} />
 
                         <button
-                            className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                            className="w-full inline-flex items-center justify-center px-4 py-2 bg-blue-600 border border-transparent
+                            rounded-md font-semibold capitalize text-white hover:bg-blue-700 active:bg-blue-700 focus:outline-none
+                            focus:border-blue-700 focus:ring focus:ring-blue-200 disabled:opacity-25 transition"
                         >Create Account
                         </button>
 
