62af1848e8496bb6b76d88501195fa40325fee43
Wenke Quarteer <wendyquarteer@outlook.com>
2022-08-26 08:54:19 +0200

    added field for email in checkout + validation

--- webshop-frontend/src/routes/CheckoutPage.js
+++ webshop-frontend/src/routes/CheckoutPage.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const CheckoutPage = () => {
+
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState(null);
+
+    function isValidEmail(email) {
+        return /\S+@\S+\.\S+/.test(email);
+    }
+
+    const handleEmailChange = event => {
+        if (!isValidEmail(event.target.value)) {
+            setError('Email is invalid');
+        } else {
+            setError(null);
+        }
+        setMessage(event.target.value);
+    };
+
+
     return (
         <div>
             <div className="max-w-7xl mx-auto pt-16 pb-24 px-4 sm:px-6 lg:px-8">
@@ -117,6 +135,22 @@ const CheckoutPage = () => {
                                         <div className="mt-1">
                                             <input type="text" name="phone" id="phone" autocomplete="tel"
                                                    className="py-2 px-3 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"/>
+                                        </div>
+                                    </div>
+
+                                    {/*--EMAIL--*/}
+                                    <div className="sm:col-span-2">
+                                        <label for="email"
+                                               className="block text-sm font-medium text-gray-700">Email</label>
+                                        <div className="mt-1">
+                                            <input type="text" name="email" id="email" autocomplete="email"
+                                                   className="py-2 px-3 block w-full border border-gray-300 rounded-md
+                                                   shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"v
+                                                   alue={message}
+                                                   onChange={handleEmailChange}
+                                                   required/>
+                                            {error}
+
                                         </div>
                                     </div>
 
