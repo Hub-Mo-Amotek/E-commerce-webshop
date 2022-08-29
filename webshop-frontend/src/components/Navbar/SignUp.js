ac27340ebe795ffdd363d576d27792c8b4d515cb
Hub-Mo <Mohamed.elja@hotmail.com>
2022-08-29 12:36:57 +0200

    added functionality to signup change myaccount page

--- webshop-frontend/src/components/Navbar/SignUp.js
+++ webshop-frontend/src/components/Navbar/SignUp.js
@@ -4,22 +4,40 @@ import {Link} from "react-router-dom";
 
 const SignUpButton = () => {
     let [logged, setLogged] = useState();
+    let [name, setName] = useState("");
+    let [path, setPath] = useState("");
+
 
     useEffect(()=>{
         let auth = localStorage.getItem('id');
 
         if(auth){
-                setLogged('hidden');
+            changeItOne()
+
               }else{
-                setLogged('inline-flex items-center px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-md');
+                changeItTwo()
+
               }
 
 }, []);
 
+const changeItOne = () => {
+    setPath("/my-account");
+    setName("my account");
+}
+const changeItTwo = () => {
+    setPath("/SignUp");
+    setName("sign up");
+}
+
+console.log('path', path);
+console.log('name', name);
+
+
     return (
-        <Link to="/SignUp">
-            <button type="button" className={logged}>
-                Sign up
+        <Link to={path}>
+            <button type="button" className='inline-flex items-center px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm font-medium rounded-md'>
+                {name}
             </button>
         </Link>
     )
