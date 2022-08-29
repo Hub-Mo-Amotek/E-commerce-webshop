cc5ad441996b7223c5ec2a05024df1caa1a14438
Hub-Mo <Mohamed.elja@hotmail.com>
2022-08-29 13:56:02 +0200

    testing communication with backend for my account page

--- webshop-frontend/src/components/homepage/NewProducts.js
+++ webshop-frontend/src/components/homepage/NewProducts.js
@@ -60,8 +60,22 @@ let test=[];
                                     </h2>
                                     <p className="text-base font-bold">&euro; {product.price}</p>
                                 </div>
+                                <button type="submit" className="add-to-cart flex items-center justify-center
+                                            w-full px-8 py-4 mt-4 rounded-sm bg-yellow-500" onClick={() => addToCart(product.id)}>
+              <span className="text-sm font-medium">
+                                    Add to Cart
+                                    </span>
+              <svg
+                  className="w-5 h-5 ml-1.5"
+                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                  stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                      d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"/>
+              </svg>
+            </button>
                             </div>
                         </Link>
+                        
                         )}
                     </div>
                 </div>
